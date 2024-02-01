60feaf0701277f592f43b092b016966c3e189124
will-pettifer <up2206186@myport.ac.uk>
2024-02-01 10:28:31 +0000

    idk

--- Assets/Scripts/Other/CameraTracker.cs
+++ Assets/Scripts/Other/CameraTracker.cs
@@ -7,31 +7,18 @@ public class CameraTracker : MonoBehaviour
     public Transform robot;
     Vector3 cursor;
     Vector3 target;
-    GameObject cameraLocation;
-    float distance;
     float mod;
 
     void Start()
     {
-        cameraLocation = GameObject.Find("Camera Location 0");
+        mod = 7;
     }
 
     void FixedUpdate()
     {
         cursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        distance = Vector3.Magnitude(cameraLocation.transform.position - robot.position);
-
-        if (distance < 9)
-        { 
-            target = cameraLocation.transform.position;
-            mod = distance / 2;
-        }
-        else
-        {
-            target = cursor;
-            mod = 7;
-        }
+        target = cursor;
 
         transform.position = robot.position + ((target - robot.position) / mod);
     }
