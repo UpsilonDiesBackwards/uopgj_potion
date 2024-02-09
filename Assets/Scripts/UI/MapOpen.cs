fa30742b98782b40ac1eabb52a27ac62d5e0202d
KazThomas <mr.firebass@gmail.com>
2024-02-09 13:33:42 +0000

    Introduced Map and Walk Animation
    Map script that can't be attached to the canvas UI ...dont ask me why
    Walk Animation that can't be sliced so can't be implemented yet

---
+++ Assets/Scripts/UI/MapOpen.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapOpen : MonoBehaviour
+{
+    public static bool mapIsOpen = false;
+    public GameObject mapScreen;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mapIsOpen == true)
+        {
+            OpenMap();
+        }
+        else
+        {
+            CloseMap();
+        }
+
+        if (mapIsOpen == false && Input.GetKeyDown(KeyCode.M))
+        {
+            OpenMap();
+        }
+    }
+    void OpenMap()
+    {
+        mapScreen.SetActive(true);
+        Time.timeScale = 1f;
+        mapIsOpen = true;
+    }
+
+    void CloseMap()
+    {
+        mapScreen.SetActive(false);
+        Time.timeScale = 1f;
+        mapIsOpen = false;
+    }
+}
